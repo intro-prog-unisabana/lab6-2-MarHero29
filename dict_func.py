9f5af6d94f580e6309eb81c4df93f2b1758dedd6
Mariana Hernandez <marianahero@unisabana.edu.co>
2026-04-08 20:04:47 -0500

    done lab6

--- dict_func.py
+++ dict_func.py
@@ -1,1 +1,19 @@
-# Write your code here!
+def employee_print(employee_info):
+    name = employee_info.get("Name", "N/A")
+    salary = employee_info.get("Salary", "N/A")
+    role = employee_info.get("Role", "N/A")
+
+    print(f"Name: {name}")
+    print(f"Salary: {salary}")
+    print(f"Role: {role}")
+
+    other_info = employee_info.copy()
+    other_info.pop("Name", None)
+    other_info.pop("Salary", None)
+    other_info.pop("Role", None)
+
+    if not other_info:
+        print("No other info!")
+    else:
+        for key, value in other_info.items():
+            print(f"{key}: {value}")
--- dict_store.py
+++ dict_store.py
@@ -1,1 +1,4 @@
-# Write your code here!
+def temp_and_color(get_values):
+    temp = get_values("temp")
+    color = get_values("color")
+    return temp, color
